c2b71f02e469a9dacea692c77fb469091a8fd743
unknown <gustavo.dantas.marim@outlook.com>
2022-05-02 16:15:10 -0300

    Estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // Pegar localização do usuário
+  const [location, setLocation] = useState(false);
+
+  // Guardar dados da API 
+  const [weather, setWeather] = useState(false);
+
+  // Requisição da API
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  }
+
+  // Hook para pegar as informações de geolocalização
+  useEffect(() => {
+    // Pegar localização do usuário (pelo browser)
+    navigator.geolocation.getCurrentPosition((position) => {
+      // recebe a latitude e longitude do usuário
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    })
+  }, []);
+
+  // Tratamento de erro => não foi possível pegar a localização
+  if (location === false) {
+    return (
+      <>
+        Você precisa habilitar a localização no browser...
+      </>
+    )
+  } else if (weather === false) {
+    return (
+      <>
+        Carregando o clima...
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h3>Clima nas suas Coordenadas ({weather['weather'][0]['description']})</h3>
+        <hr />
+        <ul>
+          <li>Temperatua atual: {weather['main']['temp']}º</li>
+          <li>Temperatura máxima:{weather['main']['temp_max']}º</li>
+          <li>Temepratura mínima: {weather['main']['temp_min']}º</li>
+          <li>Pressão: {weather['main']['pressure']}</li>
+          <li>Umidade: {weather['main']['humidity']}º%</li>
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
